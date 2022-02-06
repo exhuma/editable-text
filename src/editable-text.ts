2f087de4f6e1a6cf4c29fbcd393b6acd6cb71e0c
Michel Albert <michel@albert.lu>
2022-02-06 15:32:44 +0100

    First MVP

--- src/editable-text.ts
+++ src/editable-text.ts
@@ -1,9 +1,45 @@
-import { LitElement } from "lit";
-import { customElement } from "lit/decorators.js";
+import { html, LitElement } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
+import { ref, createRef, Ref } from 'lit/directives/ref.js';
 
 @customElement('editable-text')
 class EditableText extends LitElement {
+    @property()
+    value: string = ""
+
+    @state()
+    _editing = false;
+
+    inputRef: Ref<HTMLInputElement> = createRef()
+
+    _startEditing() {
+        this._editing = true;
+    }
+
+    _cancelEdit() {
+        this._editing = false;
+    }
+
+    _commitEdit() {
+        if (!this.inputRef.value) {
+            throw new Error("Unable to find a reference to the text-field");
+        }
+        this.value = this.inputRef.value.value;
+        this._editing = false;
+        this.dispatchEvent(new CustomEvent("change", { detail: { newValue: this.value } }));
+    }
+
     override render() {
-        return "Hello World!";
+        if (this._editing) {
+            return html`
+            <input ${ref(this.inputRef)} type="text" value="${this.value}" />
+            <a @click="${this._commitEdit}">commit</a>
+            <a @click="${this._cancelEdit}">cancel</a>
+            `
+        }
+        return html`
+        <span>${this.value}</span>
+        <a @click="${this._startEditing}">edit</a>
+        `
     }
 }
